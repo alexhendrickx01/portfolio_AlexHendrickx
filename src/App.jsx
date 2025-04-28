dc7d36e5399241a5b715bec7116816c954b453ec
alexhendrickx01 <r0892196@student.thomasmore.be>
2025-04-28 14:17:37 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
+import { LoadingScreen } from './components/LoadingScreen'
+import { Navbar } from '/src/components/NavBar'
+import { MobileMenu } from './components/MobileMenu'
+import { Home } from './components/sections/Home'
+import { About } from './components/sections/About'
+import { Projects } from './components/sections/Projects'
+import { Contact } from './components/sections/Contact'
+
+import { Routes, Route } from 'react-router-dom'
+import { Cv } from './components/sections/Cv'
+import { ProjectDetails } from './components/sections/ProjectDetails'
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
@@ -14,19 +25,21 @@ function App() {
   );
 
   return (
-    <Router basename="/portfolio_AlexHendrickx"> {/* Voeg de basename toe */}
+    <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
-        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/cv" element={<Cv />} />
-          <Route path="/projects/:projectId" element={<ProjectDetails />} />
-        </Routes>
-      </div>
-    </Router>
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}        >
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/cv" element={<Cv />} />
+            <Route path="/projects/:projectId" element={<ProjectDetails />} />
+          </Routes>
+
+        </div>
+      
+    </>
   );
 }
 
-export default App;
+export default App
